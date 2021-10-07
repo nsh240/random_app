4bf6da541d3032a61834d66c1f0da60253703f7e
nsh240 <74507655+nsh240@users.noreply.github.com>
2021-10-07 06:36:57 -0400

    Fixed bug

--- random_number.py
+++ random_number.py
@@ -1,8 +1,16 @@
+from __future__ import print_function
 import random
 
-range=[]
-range_start = input("Please enter the beginning of the range: ")
-range_end = input("Please enter the end of the range: ")
-print(range_start,range_end)
-random_num= random.randint(range_start, range_end)
-print(random_num)
+range_start = int(input("Please enter the beginning of the number range: "))
+
+range_end = int(input("Please enter the end of the number range: "))
+
+if range_end<range_start:
+    print("please enter an ending number that is greater than the starting number.")
+    print(range_start,range_end)
+else:
+    random_num= random.randint(range_start, range_end)
+    print('Your random number is:  ', random_num)
+
+
+
